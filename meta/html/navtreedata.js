a81ad5f0c7ebb44af84ded024f0fffc334507374
Dmytro Kovalchuk <dmytro.kovalchuk@plvision.eu>
2025-11-11 21:34:50 +0200

    Initial commit
    Signed-off-by: Dmytro Kovalchuk <dmytro.kovalchuk@plvision.eu>

---
+++ meta/html/navtreedata.js
@@ -0,0 +1,117 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "SAI Metadata", "index.html", [
+    [ "Modules", "modules.html", "modules" ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", "globals_type" ],
+        [ "Enumerations", "globals_enum.html", "globals_enum" ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"group__SAIACL.html#ga44dd0ccceb1391cd77f19ca4de92bef2",
+"group__SAIACL.html#gga25441fd99b0ba7ffdc071c8759912145a5a8d9a68ca2b5df209085628d3f65def",
+"group__SAIACL.html#gga524081259f98085c419508a47af596ffa2fa0f03926ccc262520c86f9692f5039",
+"group__SAIACL.html#ggadca7ba1d7fe94b0f924566dd3adca1eba37c9b7afc8c9961ed055d2f81c13fa8c",
+"group__SAIARSPROFILE.html#gaaa781f99276391d892db15720ae0743b",
+"group__SAIBFD.html#gga447e86ac278a7a786dba33d606901763ae3d6fa44a955004e357b247e360dcce8",
+"group__SAIBUFFER.html#ga0e7c89734911fb80c0cfa4993a89d1c4",
+"group__SAICOUNTER.html#ga0721458715bb4fff216303ff90c769ce",
+"group__SAIDEBUGCOUNTER.html#ggaeead349562594f067067f2a38e8157c4af482e59a32893b22d48e31a6e0823734",
+"group__SAIEXPERIMENTALBMTOR.html#gade02a711f21f915bebdb3fe608148e82",
+"group__SAIEXPERIMENTALDASHENI.html#ga6b52de7995ffc94f096c8c2a785c8667",
+"group__SAIEXPERIMENTALDASHENI.html#gga4d52ac1ae893cfe7c52d0c6b6737b47aacd4f9fe52a7fd19a4bcca0760e4ebb16",
+"group__SAIEXPERIMENTALDASHHA.html#ga7ba1319563c58887800a5271918510ad",
+"group__SAIEXPERIMENTALDASHMETER.html#ggaa2952ae08959da4095727897d8f98281aac92ba6c8e4dce0cdd548fd740c3c6b1",
+"group__SAIEXPERIMENTALDASHPAVALIDATION.html#ga410e8c6145a6f7ae4704072d5b27a4f7",
+"group__SAIFDB.html#ga4a60a6a6e2db9eefea68e8f1aae405d0",
+"group__SAIHASH.html#ggafcc31874ad58eb9d8c620352bd1e5b4eaf0266386ace70c2438533a2950ee54e8",
+"group__SAIHOSTINTF.html#gga99d83686daf81153e6d5a161fbbda0c9a546c215e13f986140b0ff289132edf91",
+"group__SAIICMPECHO.html#ggae6ddabb7d7de6422d5f2002f93817bb8a8f35df53c2f43a892fa3cc2b0f4abffc",
+"group__SAIIPSEC.html#gga186432b79ed39ae72d2b85a3df969c5badf4f39591062d93452b9fa52b6a2f129",
+"group__SAIL2MCGROUP.html#gab6bdabd2bbb7f357b5211484f666e65c",
+"group__SAIMACSEC.html#gga2bffeeed497dc78a11936cc78e3b0cd3a07c06a47a99698b8d83663cba6d373ef",
+"group__SAIMCFDB.html#ga0393c0107b3211c138e928f576f78706",
+"group__SAIMETADATATYPES.html#gga9cd0e8e2efb1f7fdcd554a669d27ab3ba8cb33ad734424410afe90d81cb6c15ad",
+"group__SAIMPLS.html#ga2930a9273cdb4bc4a2a28473ca2829f1",
+"group__SAINEIGHBOR.html#ggaae5e1a835bf95e5dab9f146c57b07e78aea6d60a91234154b8b485722913df063",
+"group__SAINEXTHOPGROUP.html#ggafafc1eb7d1f7a57891992c5d9e936439abe6dc6afffe7bd300fd36edf2532ed12",
+"group__SAIPORT.html#ga6cd1447cff30fb8989b294f0493018d8",
+"group__SAIPORT.html#gga189a5769756b00c66d71e544d9bbb796a3bdfe6cdef94ef50b9a0e653146a4be2",
+"group__SAIPORT.html#gga189a5769756b00c66d71e544d9bbb796ab5e602dbb1a6645b72cafc61f88276dc",
+"group__SAIPORT.html#gga24a16eaa93d52508959540d89b5738e6a6a8e97798d64f6c21d75ea44c54a863b",
+"group__SAIPORT.html#ggaa1d1819db0e8afd6c339bdb797d244f4a47dc9258184e1d1af4e93f54cb3831e5",
+"group__SAIPORT.html#ggaf6a4c24bfa563ea78247c935714eff4ba924c73043155a7718998d514b6cdbbbd",
+"group__SAIQOSMAPS.html#ga9173af471a3cd4e9005383b5c7f1c385",
+"group__SAIQUEUE.html#ggad01a80a619ce999c5db4d67bc3ad5ee6a7919fade45968bf23fce95af8af26582",
+"group__SAIRPFGROUP.html#gga8acbebc8aadb641e8ae6e5a778fa712baa102ea365c18d1c6ec40bba182c931d0",
+"group__SAISERIALIZE.html#gadc2b1614ce2d6a9ea9ef4b524bfd8c50",
+"group__SAISTATUS.html#gaa80016e804df38f44ec9ea6ac261d0fd",
+"group__SAISWITCH.html#gade67e068ba153b42547038a50c30fe86",
+"group__SAISWITCH.html#gga96df0871b3925f973f4a9079ffb3e634a14b4d57972fb5309e8686f66cf8e13a5",
+"group__SAISWITCH.html#gga96df0871b3925f973f4a9079ffb3e634a860f01dc23cde3a8964ceca6f1f247cb",
+"group__SAISWITCH.html#gga96df0871b3925f973f4a9079ffb3e634afa1306f907d2eb7cf6d252af4e9d3516",
+"group__SAITAM.html#ga5c7fbd12bbcb71be98273d3851d0411f",
+"group__SAITAM.html#gga4234dceb3d66e8358169d734f19e601da98137998aa803794fc6e82373c79bea7",
+"group__SAITAM.html#ggabd491a69150e3e9ebe3b330a3f663491a7e16f404799a198745cac4023ce5d09c",
+"group__SAITUNNEL.html#gga1d47564379bb47d99433f81a3dfae386a329dd79d5b02cfa80f844a6ef21790b6",
+"group__SAITWAMP.html#ggaa363f42736b57a738c862f590a2cf0daa0fec4c377403c3f5f120de598a6fd22d",
+"group__SAITYPES.html#ga86bc6f40adc2a4ee7caa899b81f9dac9",
+"group__SAITYPES.html#gga4450320ede120f84efcd17a09c58e29fa9a1b25e350304c5e84bdeef59b86ff88",
+"group__SAIUDF.html#gga36a0e72a1cb059bdc407096aac81c900a92456ddc6a00e94bc5105a290b489deb",
+"group__SAIWRED.html#ggae3d96cebd897bc0ef674a81de5248c89a180b5e43a4a1d8fa17c9ad8bd0196524",
+"saipoe_8h.html#a7a045ebf67118ae77416ab6e1217aa61",
+"saitypesextensions_8h.html#af7f2d7d34ddad9279635e70aed2ad3c2a363d68a628b1330edb8bebae2b1bd45d",
+"struct__sai__buffer__api__t.html#ad998621fb19c64d7716a69b426c22996",
+"struct__sai__enum__metadata__t.html#a1cea288c3ba6d7925806bd8da59ea87e",
+"struct__sai__macsec__api__t.html#ac8bff9a6380315fc8b52d1ddb9d2edd8",
+"struct__sai__port__frequency__offset__ppm__list__t.html",
+"struct__sai__switch__api__t.html#aa4f95c0b03769d2d73af85ba4a6b66ec",
+"union__sai__attribute__value__t.html#a36756268c26c99936354c148b144318c"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
