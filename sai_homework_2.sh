7d617e68d0239344b922aaf4418ccb9be690f9db
Dmytro Kovalchuk <dmytro.kovalchuk@plvision.eu>
2025-11-19 15:57:30 +0200

    Add rebuilding and installing SAI library
    Signed-off-by: Dmytro Kovalchuk <dmytro.kovalchuk@plvision.eu>

--- sai_homework_2.sh
+++ sai_homework_2.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+cd stub
+
+./autogen.sh
+./configure --prefix=/usr
+make && sudo make install
+
+cd..
+
 gcc -o final_test -I ./stub/inc/ -I /usr/include/sai -L /usr/lib test_final.c -lsai
 ./final_test
